d429a1f9318e638157dccdf9dd195d4b116da86f
1vipin <vipin.techindustan@gmail.com>
2018-11-27 18:37:08 +0530

    some changes in design

--- routes/invoices.js
+++ routes/invoices.js
@@ -13,118 +13,243 @@ function ensureAuthenticated(req, res, next){
 	}
 	res.redirect('/Auth/login');
 }
-
 router.get('/',ensureAuthenticated,(req, res) =>{
-   Invoice.find()
-    .select("name email contactnumber city address zipcode")
-    .exec()
-    .then(invoices => {
-        console.log(invoices,'invoices')
-        res.render('invoices/list', { invoices })
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error: err
-        });
-    });
-});
-
-
-
-router.get('/add', (req, res) => {
-    res.render('invoices/add', {
-        title: 'Add'
-    })
+    Invoice.find()
+     .select("name email contactnumber city address zipcode")
+     .exec()
+     .then(invoices => {
+         console.log(invoices,'invoices')
+         res.render('invoices/list', { invoices })
+     })
+     .catch(err => {
+         console.log(err);
+         res.status(500).json({
+             error: err
+         });
+     });
+    
 })
-
-router.post('/add', (req, res) => {
-    let { isValid, errors} = validator(req.body);
-    console.log(isValid, errors)
-	   if (!isValid) {s
-		res.render('invoices/add', {
+  
+ 
+ router.get('/add', (req, res)=>{
+     res.render('invoices/add',{
+         title: 'Add'
+     })
+ })
+ 
+ router.post('/add', (req, res)=>{
+ 
+     let { isValid, errors} = validator(req.body);
+     console.log(isValid, errors)
+    
+     if (!isValid) {
+       
+         res.render('invoices/add', {
             err: errors,
             invoice: { name: req.body.name, email: req.body.email, contactnumber: req.body.contactnumber,
-                 city: req.body.city, address: req.body.address, zipcode: req.body.zipcode}
-        });
-    }
-    else{
-        var invoice = new Invoice({
-            _id: new mongoose.Types.ObjectId(),
-            name : req.body.name,
-            email: req.body.email,
-            contactnumber: req.body.contactnumber,
-            city: req.body.city,
-            address: req.body.address,
-            zipcode: req.body.zipcode
-        });
-    
+            city: req.body.city, address: req.body.address, zipcode: req.body.zipcode}
+         });
+     } else {
+         
+         var invoice = new Invoice({
+             _id: new mongoose.Types.ObjectId(),
+             name : req.body.name,
+             email: req.body.email,
+             contactnumber: req.body.contactnumber,
+             city: req.body.city,
+             address: req.body.address,
+             zipcode: req.body.zipcode
+         });
         invoice
-        .save()
-        .then(result => {
-            console.log(result);        
-            res.redirect('/payment'),
-            req.flash('Invoice created');
-        })
-        .catch(err => {
-            console.log(err);
-            res.redirect('/invoices/add')
-        });
-    }
-});
-
-
-
-router.get('/edit/:id', async (req, res)=>{
-    const invoices = await getInvoice(req.params.id);
-    res.render('invoices/edit',{
-        title: 'Edit',
-        invoices
-    })
-});
-
-async function getInvoice(id) {
-    try{
-        const invoice = await Invoice.findOne({ _id: id }).exec();
-        return invoice;
-    } catch(err) {
-        throw err;
-    }
-}
-
-router.put('/edit/:id', (req, res)=>{
-   Invoice.update({_id: req.params.id},
-        { $set:{ 
+         .save()
+         .then(result => {
+            console.log(result);
+           res.redirect('/invoices'),
+            req.flash('message','invoices Created');
+         })
+            .catch(err =>{
+                console.log(err);
+                res.redirect('/invoices/add')
+            });
+         
+         }
+     });
+        
+         router.get('/edit/:id', async (req, res)=>{
+             const invoices = await getInvoice(req.params.id);
+             res.render('invoices/edit',{
+                 title: 'Edit',
+                 invoices
+             })
+         });  
+     
+       
+ async function getInvoice(id) {
+     try{
+         const invoice = await Invoice.findOne({ _id: id }).exec();
+         return invoice;
+     } catch(err) {
+         throw err;
+     }
+ }
+ 
+ router.put('/edit/:id', (req, res)=>{
+     let { isValid, errors} = validator(req.body);
+     console.log(isValid, errors)
+     
+     if (!isValid) {
+     
+         res.render('invoices/edit', {
+             err: errors,
+             invoice: { name: req.body.name, email: req.body.email, contactnumber: req.body.contactnumber,
+                city: req.body.city, address: req.body.address, zipcode: req.body.zipcode}
+         });
+     } else{
+    Invoice.update({_id: req.params.id},
+         { $set:{ 
             name : req.body.name,
             email: req.body.email,
             contactnumber: req.body.contactnumber,
             city: req.body.city,
             address: req.body.address,
             zipcode: req.body.zipcode
-        }
-    })
-    .exec()
-    .then(res => {
-        res.redirect('/invoices');
-        req.flash('Invoice Updated');
-    })
-    .catch(err => {
-        res.redirect('/invoices');
-    })
-})
+             
+         }
+     })
+     .exec()
+     .then(res => {
+         res.redirect('/invoices');
+         req.flash('invoice Updated');
+     })
+     .catch(err => {
+         res.redirect('/invoices');
+     })
+   }
+ })
+ 
+ 
+ router.get('/delete/:id', (req,res)=>{
+     Invoice.remove({ _id: req.params.id})
+     .exec()
+     .then(result => {
+         res.redirect('/invoices')
+     })
+     .catch(err => {
+         console.log(err);
+         res.redirect('invoices/list')
+     });
+ })
+// router.get('/',ensureAuthenticated,(req, res) =>{
+//    Invoice.find()
+//     .select("name email contactnumber city address zipcode")
+//     .exec()
+//     .then(invoices => {
+//         console.log(invoices,'invoices')
+//         res.render('invoices/list', { invoices })
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.status(500).json({
+//             error: err
+//         });
+//     });
+// });
 
 
-router.get('/delete/:id', (req,res)=>{
-    Invoice.remove({ _id: req.params.id})
-    .exec()
-    .then(result => {
-        res.redirect('/invoices')
-    })
-    .catch(err => {
-        console.log(err);
-        res.redirect('invoices/list')
-    });
-})
+
+// router.get('/add', (req, res) => {
+//     res.render('invoices/add', {
+//         title: 'Add'
+//     })
+// })
+
+// router.post('/add', (req, res) => {
+//     let { isValid, errors} = validator(req.body);
+//     console.log(isValid, errors)
+// 	   if (!isValid) {s
+// 		res.render('invoices/add', {
+//             err: errors,
+//             invoice: { name: req.body.name, email: req.body.email, contactnumber: req.body.contactnumber,
+//                  city: req.body.city, address: req.body.address, zipcode: req.body.zipcode}
+//         });
+//     }
+//     else{
+//         var invoice = new Invoice({
+//             _id: new mongoose.Types.ObjectId(),
+//             name : req.body.name,
+//             email: req.body.email,
+//             contactnumber: req.body.contactnumber,
+//             city: req.body.city,
+//             address: req.body.address,
+//             zipcode: req.body.zipcode
+//         });
+    
+//         invoice
+//         .save()
+//         .then(result => {
+//             console.log(result);        
+//             res.redirect('/payment'),
+//             req.flash('Invoice created');
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.redirect('/invoices/add')
+//         });
+//     }
+// });
+
+
+
+// router.get('/edit/:id', async (req, res)=>{
+//     const invoices = await getInvoice(req.params.id);
+//     res.render('invoices/edit',{
+//         title: 'Edit',
+//         invoices
+//     })
+// });
+
+// async function getInvoice(id) {
+//     try{
+//         const invoice = await Invoice.findOne({ _id: id }).exec();
+//         return invoice;
+//     } catch(err) {
+//         throw err;
+//     }
+// }
+
+// router.put('/edit/:id', (req, res)=>{
+//    Invoice.update({_id: req.params.id},
+//         { $set:{ 
+//             name : req.body.name,
+//             email: req.body.email,
+//             contactnumber: req.body.contactnumber,
+//             city: req.body.city,
+//             address: req.body.address,
+//             zipcode: req.body.zipcode
+//         }
+//     })
+//     .exec()
+//     .then(res => {
+//         res.redirect('/invoices');
+//         req.flash('Invoice Updated');
+//     })
+//     .catch(err => {
+//         res.redirect('/invoices');
+//     })
+// })
+
+
+// router.get('/delete/:id', (req,res)=>{
+//     Invoice.remove({ _id: req.params.id})
+//     .exec()
+//     .then(result => {
+//         res.redirect('/invoices')
+//     })
+//     .catch(err => {
+//         console.log(err);
+//         res.redirect('invoices/list')
+//     });
+// })
 
 // Validation function//
 function validator(data) {
